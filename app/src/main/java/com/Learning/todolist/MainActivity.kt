704f31f3e63938861477b1f78d4bca14a6a41682
Srignan Paruchuru <Srignan7@gmail.com>
2023-03-06 20:57:51 -0500

    Worked on the migration changes and finally finished

--- app/src/main/java/com/Learning/todolist/MainActivity.kt
+++ app/src/main/java/com/Learning/todolist/MainActivity.kt
@@ -3,29 +3,33 @@ package com.Learning.todolist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
+import com.Learning.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var todoAdapter: TodoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         todoAdapter = TodoAdapter(mutableListOf())
 
-        rvTodoItems.adapter = todoAdapter
-        rvTodoItems.layoutManager = LinearLayoutManager(this)
+        binding.rvTodoItems.adapter = todoAdapter
+        binding.rvTodoItems.layoutManager = LinearLayoutManager(this)
 
-        btnAddTodo.setOnClickListener {
-            val todoTitle = etTodoTitle.text.toString()
+        binding.btnAddTodo.setOnClickListener {
+            val todoTitle = binding.etTodoTitle.text.toString()
             if(todoTitle.isNotEmpty()) {
                 val todo = Todo(todoTitle)
                 todoAdapter.addTodo(todo)
-                etTodoTitle.text.clear()
+                binding.etTodoTitle.text.clear()
             }
         }
-        btnDeleteDoneTodos.setOnClickListener {
+
+        binding.btnDeleteDoneTodos.setOnClickListener {
             todoAdapter.deleteDoneTodos()
         }
     }
